53d80737f42a8cf095c419175737d42e1ceacd78
PatrycjaD <p.dabro93@gmail.com>
2020-02-22 16:03:21 +0100

    kNN

--- adult_train_sets.py
+++ adult_train_sets.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn import datasets, metrics
-
+from sklearn import neighbors
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("Adult_train.tab", delimiter="\t")
 df.drop(index=0, inplace=True)
@@ -16,8 +16,18 @@ corr_matrix = df.corr().abs()
 upper = corr_matrix.where(np.triu(np.ones_like(corr_matrix), k=1).astype(np.bool))
 to_drop = [column for column in upper.columns if any(upper[column] > 0.7)]
 
-train, test = train_test_split(df, test_size=0.2, shuffle=True)
 
-train.to_csv("train.csv", sep = ";", encoding = "utf-8", index = False, header = False)
-test.to_csv("test.csv", sep = ";", encoding = "utf-8", index = False, header = False)
+n_neighbors = 10
+
+
+df_original = df
+labels = df_original.pop("y")
+scaler = StandardScaler()
+df = scaler.fit_transform(df_original)
+model = neighbors.KNeighborsClassifier(n_neighbors, weights="distance")
+X_train, X_test, y_train, y_test = train_test_split(df, labels, test_size=0.3, shuffle=True)
+model.fit(X_train, y_train)
+a = model.score(X_test, y_test)
+print(a)
+
 
